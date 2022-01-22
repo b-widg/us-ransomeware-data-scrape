89881c91f49695056ff1d545327d2fd48442ac4e
Brian Widgeon <b-widg@users.noreply.github.com>
2022-01-22 13:28:16 -0800

    Complete - includung results as JSON and csv in scrape_results

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const { JSDOM } = require('jsdom');
+const ObjectsToCsv = require('objects-to-csv-file');
 
 const wait = async (milliseconds) => {
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
@@ -70,18 +72,23 @@ const getData = async () => {
 
       tableRows.forEach((row) => {
         const tableData = row.querySelectorAll('td');
-        attackDate = tableData[0].textContent;
-        entity = tableData[1].textContent;
-        city = tableData[2].textContent.split(',')[0]; // Pull city only from City, ST
-        entitType = tableData[3].textContent;
-        ransomePaid = tableData[4].textContent;
-        amountPaid = parseInt(tableData[5].textContent.slice(1)) || ''; // Remove $ from string and convert to Int
-        foiaReq = tableData[6].textContent;
-        avBefore = tableData[7].textContent.split(', '); // Convert to array
-        avAfter = tableData[8].textContent.split(', '); // Convert to array
+        const attackYear = parseInt(tableData[0].textContent.split('-')[0]); // Original date format was 'YYYY-MM'
+        const attackMonth = parseInt(tableData[0].textContent.split('-')[1]);
+        const entity = tableData[1].textContent;
+        const city = tableData[2].textContent.split(',')[0]; // Pull city only from City, ST
+        const entitType = tableData[3].textContent;
+        const ransomePaid = tableData[4].textContent;
+        const amountPaid = parseInt(tableData[5].textContent.slice(1)) || ''; // Remove $ from string and convert to Int
+        const foiaReq = tableData[6].textContent;
+        const avBefore = tableData[7].textContent;
+        const avAfter = tableData[8].textContent;
+        const articleLink = tableData[1]
+          .querySelector('a')
+          .getAttribute('href');
 
         const attackDetail = {
-          attackDate,
+          attackYear,
+          attackMonth,
           entity,
           city,
           state,
@@ -91,6 +98,7 @@ const getData = async () => {
           foiaReq,
           avBefore,
           avAfter,
+          articleLink,
         };
         attackDetails = [...attackDetails, attackDetail];
       });
@@ -99,3 +107,20 @@ const getData = async () => {
   }
   return attackDetails;
 };
+
+(async () => {
+  const attackDetails = await getData();
+
+  // Save as JSON to file
+  fs.writeFile(
+    './scrape_results/attack-details.json',
+    JSON.stringify(attackDetails),
+    (error) => {
+      if (error) console.log('ERROR - Write JSON file: ', error);
+    }
+  );
+
+  // Save as CSV
+  const csv = new ObjectsToCsv(attackDetails);
+  await csv.toDisk('./scrape_results/attack-details.csv');
+})();
